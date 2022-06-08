2c5d3dfdbf6c934c876a2d0c0b2c02a69493f069
sandeep <sandeepbisht045>
2022-06-08 13:31:45 +0530

    18th

---
+++ addons/om_hospital/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hospital Management',
+    'version': '12.0.1.0.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Module for managing the hospitals',
+    'sequence': '10',
+    'license' : 'AGPL-3',
+    'author': 'Odoo Mates',
+    'maintainer': 'Odoo Mates',
+    'website': 'odoomates.com',
+    'depends': ['mail','sale',],
+    'demo': [],
+    'data': ["views/patient.xml","data/sequence.xml","views/appointment.xml"
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ addons/om_hospital/models/patient.py
@@ -0,0 +1,52 @@
+from odoo import models,fields,api,_
+from odoo.exceptions import  ValidationError
+
+class SaleOrderInherit(models.Model):
+    _inherit='sale.order'
+    patient_name=fields.Char(string="Patient Name")
+
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Patient Record'
+    patient_name = fields.Char(string='Name',required=True,track_visibility="always")
+    patient_age = fields.Integer('Age',track_visibility="always")
+    patient_contact = fields.Integer('Contact')
+    name = fields.Char(string='Test')
+    name_seq = fields.Char(string='Patient ID', required=True, copy=False, readonly=True,
+                           index=True, default=lambda self: _('New'))
+    notes = fields.Text(string="Registration Note")
+
+    @api.constrains("patient_age")
+    def check_age(self):
+        for rec in self:
+            if rec.patient_age<=5:
+                raise  ValidationError(_("The age must be greater than 5"))
+
+    @api.depends('patient_age')
+    def set_age_group(self):
+        for rec in self:
+            if rec.patient_age:
+                if rec.patient_age < 18:
+                    rec.age_group = 'minor'
+                else:
+                    rec.age_group = 'major'
+
+    gender = fields.Selection([
+        ('male', 'Male'),
+        ('fe_male', 'Female'),
+    ], default='male', string="Gender")
+
+    age_group = fields.Selection([
+        ('major', 'Major'),
+        ('minor', 'Minor'),
+    ], string="Age Group", compute='set_age_group', store=True)
+
+    @api.model
+    def create(self, vals):
+        if vals.get('name_seq', _('New')) == _('New'):
+            vals['name_seq'] = self.env['ir.sequence'].next_by_code('hospital.patient.sequence') or _('New')
+        result = super(HospitalPatient, self).create(vals)
+        return result
+
