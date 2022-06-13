698240b3c50e4c5d2f342edbd763a80f60095d89
sandeep <sandeepbisht045>
2022-06-13 12:13:29 +0530

    50

--- addons/om_hospital/wizards/create_appointment.py
+++ addons/om_hospital/wizards/create_appointment.py
@@ -11,4 +11,5 @@ class CreateAppointment(models.TransientModel):
             'appointment_date': self.appointment_date,
             'notes': 'Created From The Wizard/Code'
         }
+        self.patient_id.message_post(body='Test string', subject="Appointment Creation")
         self.env['hospital.appointment'].create(vals)
